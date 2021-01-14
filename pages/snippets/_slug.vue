8bd0bac69d4d5f3515986a42319e387e6a0c11d3
jasenmichael <jasen@ready-builder.com>
2021-01-14 17:37:09 -0600

    chore: create snippets _slug page

---
+++ pages/snippets/_slug.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <SnippetView :snippet="snippet" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const snippet = await $content('snippets', params.slug).fetch()
+    return { snippet }
+  },
+}
+</script>
