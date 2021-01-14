695890f422509aa48528da9d925a91b54bd036e7
jasenmichael <jasen@ready-builder.com>
2021-01-14 17:32:53 -0600

    chore: create index page

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="mx-auto container p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-6"
+  >
+    <div
+      v-for="(snippet, i) in snippets"
+      :key="i"
+      class="rounded-lg border-red-900 border-2"
+    >
+      <nuxt-link :to="`/snippets/${snippet.slug}`">
+        <SnippetCard :snippet="snippet" />
+      </nuxt-link>
+      <!-- <pre>{{ snippets }}</pre> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    const snippets = await $content('snippets')
+      .only([
+        'title',
+        'slug',
+        'description',
+        'tags',
+        'categories',
+        'author',
+        'image',
+        'updatedAt',
+        'author_img',
+      ])
+      .fetch()
+      .catch((err) => {
+        console.error(err)
+        error({ statusCode: 404, message: 'WTF! Page not found' })
+      })
+    return { snippets }
+  },
+  created() {
+    console.log(this.$route)
+  },
+}
+</script>
