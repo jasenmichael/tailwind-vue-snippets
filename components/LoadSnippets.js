b40b4f619e49f2a173a978191229418d81f8199f
jasenmichael <jasen@ready-builder.com>
2021-01-14 17:30:55 -0600

    chore: generated load snippets script

---
+++ components/LoadSnippets.js
@@ -0,0 +1,7 @@
+export default {
+  AlertsWithTailwind: () => import('~/components/snippets/AlertsWithTailwind'),
+  FbComments: () => import('~/components/snippets/FbComments'),
+  GearMenu: () => import('~/components/snippets/GearMenu'),
+  GearMenu2: () => import('~/components/snippets/GearMenu2'),
+  HorizontalCard: () => import('~/components/snippets/HorizontalCard'),
+}
